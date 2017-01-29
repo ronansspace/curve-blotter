77986b7d1bcdf17594bf27a1acfa1489f3b2c0f7
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-01-29 22:43:38 +0000

    FIX trade blotter begins.

---
+++ inc/def.php
@@ -0,0 +1,6 @@
+<?php
+define(DBHOST,"localhost");
+define(DBUSER,"root");
+define(DBPASS,"bc43f15f516460e8966700a05761371e0235799a6d86ffd7");
+define(DBNAME,"cmarkets");
+?>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+
+<?php
+session_start();
+require_once('inc/def.php');
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
