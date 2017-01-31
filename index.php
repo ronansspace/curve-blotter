3cfd97b20f09879fb42991a5d7006c83b0fb30f0
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-01-31 17:45:35 +0000

    Rearrange outputs

--- index.php
+++ index.php
@@ -46,32 +46,32 @@ require_once('inc/def.php');
                     <thead>
                     <tr>
                         <th> </th>
+                        <th>Exec ID</th>
                         <th>Account</th>
-                        <th>Symbol</th>
-                        <th>AvgPx</th>
-                        <th>ClOrdID</th>
-                        <th>CumQty</th>
+                        <th>B/S</th>
+                        <th>Pair</th>
+                        <th>Exec Qty</th>
+                        <th>Exec Price</th>
+                        <th>Order Qty</th>
+                        <th>Trade Date</th>
+                        <th>Settle Date</th>
+                        <th>Order Type</th>
+                        <th>Order Status</th>
+                        <th>Exec Type</th>
+                        <th>Avg Price</th>
+                        <th>Cumul Qty</th>
+                        <th>Remain Qty</th>
                         <th>Currency</th>
-                        <th>ExecID</th>
-                        <th>LastPx</th>
-                        <th>LastQty</th>
-                        <th>OrderID</th>
-                        <th>OrderQty</th>
-                        <th>OrdStatus</th>
-                        <th>OrdType</th>
                         <th>Price</th>
-                        <th>Side</th>
-                        <th>TimeInForce</th>
-                        <th>TransactTime</th>
-                        <th>SettlDate</th>
+                        <th>Time In Force</th>
+                        <th>Transact Time</th>
                         <th>ListID</th>
-                        <th>TradeDate</th>
-                        <th>ExecType</th>
-                        <th>LeavesQty</th>
-                        <th>EffectiveTime</th>
-                        <th>NoContraBrokers</th>
-                        <th>SecondaryExecID</th>
-                        <th>SourceSystem</th>
+                        <th>Effective Time</th>
+                        <th>No. Contra Brokers</th>
+                        <th>Secondary Exec ID</th>
+                        <th>ClOrdID</th>
+                        <th>Order ID</th>
+                        <th>Source System</th>
                         </tr>
                     </tr>
                     </thead>
@@ -135,10 +135,10 @@ require_once('inc/def.php');
                         {
                             //, env - EMAIL ICON
 
-                            oTable.fnAddData([ "<input type='checkbox' name='id_trades' class='check_box' value='"+s[i][0]+"'/>", s[i][0], s[i][1], s[i][2],
-                                s[i][3], s[i][4], s[i][5], s[i][6], s[i][7], s[i][8], s[i][9],
-                                s[i][10], s[i][11], s[i][12], s[i][13], s[i][14], s[i][15], s[i][16], s[i][17], s[i][18], s[i][19], s[i][20], s[i][21], s[i][22],
-                                s[i][23], s[i][24], s[i][25], s[i][26]
+                            oTable.fnAddData([ "<input type='checkbox' name='id_trades' class='check_box' value='"+s[i][0]+"'/>", s[i][6], s[i][0], s[i][14],
+                                s[i][1], s[i][8], s[i][7], s[i][10], s[i][19], s[i][17], s[i][12],
+                                s[i][11], s[i][20], s[i][2], s[i][4], s[i][21], s[i][5], s[i][13], s[i][15], s[i][16], s[i][18], s[i][22], s[i][23], s[i][24],
+                                s[i][3], s[i][9], s[i][25]
                             ]);
 
                             var theNode = oTable.fnSettings().aoData[i].nTr;
