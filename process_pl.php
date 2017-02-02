d147b770526c2783f0080747803c755980deac68
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-02-02 09:52:38 +0000

    Add date filters

--- process_pl.php
+++ process_pl.php
@@ -8,7 +8,38 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "call get_pl";
+$qry_type = $_GET['theid'];
+
+$xtra_qry = "";
+
+$startDate = "";
+$endDate = "";
+
+$dt_today = date('Ymd');
+
+//Last 10 days
+$dt_today_ten = date('Ymd', strtotime("-10 days"));
+
+$dt_first = str_replace("/", "-", $_GET['stdate']);
+$dt_first = date('Ymd', strtotime($dt_first));
+
+$dt_sec = str_replace("/", "-", $_GET['endate']);
+$dt_sec = date('Ymd', strtotime($dt_sec));
+
+if($qry_type == 1){
+
+}else if($qry_type == 2){
+    $startDate = $dt_today;
+    $endDate = $dt_today;
+}else if($qry_type == 3){
+    $startDate = $dt_today_ten;
+    $endDate = $dt_today;
+}else if($qry_type == 4){
+    $startDate = $dt_first;
+    $endDate = $dt_sec;
+}
+
+$sql = "call get_pl('$startDate','$endDate')";
 
 $result = $conn->query($sql);
 
