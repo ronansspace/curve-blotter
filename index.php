cf0d2e7411ffe931cb81df05c8358bb80f1b6c92
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-02-02 20:08:48 +0000

    hide the table on refresh to try speed it up

--- index.php
+++ index.php
@@ -263,7 +263,7 @@ require_once('inc/def.php');
                 "iDisplayLength": 25,
                 "processing": true,
                 "scrollX": true,
-                "order": [[ 19, "desc" ]]
+                "order": [[ 18, "desc" ]]
             });
 
             var all_val = $('input[name=options]:checked').val();
