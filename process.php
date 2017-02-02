b76d253e87d76ca80c9f0d97cd1392a15f6f1eb0
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-02-02 09:11:21 +0000

    Add date filters

--- process.php
+++ process.php
@@ -28,10 +28,8 @@ if($qry_type == 1){
 }else if($qry_type == 2){
     $xtra_qry =  "and TradeDate = '$dt_today'";
 }else if($qry_type == 3){
-
-}else if($qry_type == 4){
     $xtra_qry =  " and (TradeDate >= '$dt_today_ten' and TradeDate <= '$dt_today') ";
-}else if($qry_type == 5){
+}else if($qry_type == 4){
     $xtra_qry =  " and (TradeDate >= '$dt_first' and TradeDate <= '$dt_sec') ";
 }
 
